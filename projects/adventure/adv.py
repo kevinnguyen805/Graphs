d938fa245dbc2959502078872a9a406cbb3c73fb
Kevin Nguyen <nguyenkevin225@gmail.com>
2020-03-20 09:16:34 -0700

    smaller graph maze working

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,11 +10,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,7 +29,56 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = [] # once you make the function that spits out traversal_path - test on line 52 should be done 
 
-# ADDING REVERSE - reversed_traversal_path = []
+# ADDING REVERSE 
+reversed_traversal_path = []
+
+
+
+opposite_direction = {"n":"s", "s":"n", "e":"w", "w":"e"}
+
+
+
+def find_exits(current_room, visited):
+    valid_exits = []
+    for exit in current_room.get_exits():
+        if exit in room_graph[current_room.id][1]:
+            if room_graph[current_room.id][1][exit] not in visited:
+                valid_exits.append(exit)
+    return valid_exits
+
+
+
+def traverse_maze():
+    visited = set()
+    visited.add(player.current_room.id)
+    reversed_traversal_path = []
+
+    while len(visited) < len(room_graph.keys()):
+        current_room = player.current_room.id 
+        valid_exits = find_exits(player.current_room, visited)
+        print(valid_exits)
+
+        if len(valid_exits) > 0:
+            for direction in valid_exits: 
+                visited.add(room_graph[current_room][1][direction])
+                traversal_path.append(direction)
+                reversed_traversal_path.append(opposite_direction[direction])
+                player.travel(direction)
+                break 
+            else: 
+                print(reversed_traversal_path)
+                direction = reversed_traversal_path.pop()
+                player.travel(direction)
+                traversal_path.append(direction)
+traverse_maze()
+
+
+
+
+
+
+
+
 
 
 # TRAVERSAL TEST
